a31ab8d752e62e19c0361a3dbe3f433879be6561
unknown <putri Angraeni>
2023-12-16 10:25:47 +0800

    first commit

---
+++ home.php
@@ -0,0 +1,23 @@
+<div class="page-header">
+</div>
+<div align=center>
+    <br>
+    <h1>
+        <p>Selamat Datang</p>
+    </h1>
+    <h1>
+        <p>Sistem Pendukung Keputusan Dalam Memilih Masker Wajah Organik</p>
+    </h1>
+    <h1>
+        <p>Analytical Hierarchy Process (AHP)</p>
+    </h1>
+    <br>
+    <br>
+    <h4>
+        <p>PUTRI ANGRAENI - 210210502036</p>
+    </h4>
+    <h4>
+        <p>PUTRI ANGRAENI BADAR - 210210502058</p>
+    </h4>
+
+</div>
